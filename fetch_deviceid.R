f0fbffb6dc2fe91971f9ad4b77c12048457fe06c
Gustavo Olivares <guolivar@gmail.com>
2018-05-11 15:59:44 +1200

    Slowly getting there!

--- fetch_deviceid.R
+++ fetch_deviceid.R
@@ -3,3 +3,23 @@
 #' author: "Gustavo Olivares"
 #' ---
 
+library(readr)
+library(RJSONIO)
+library(curl)
+
+# Read the secrets
+secret_hologram <- read_delim("/data/GusData/repositories/odin_gisborne2018/secret_hologram.txt", 
+                              "\t", escape_double = FALSE, trim_ws = TRUE)
+base_url <- "https://dashboard.hologram.io/api/1/devices?"
+tag <- "gisborne"
+built_url <- paste0(base_url,
+                    "orgid=",secret_hologram$orgid,"&",
+                    "tagname=",tag,"&",
+                    "apikey=",secret_hologram$apikey)
+req1 <- curl_fetch_memory(built_url)
+jreq <- fromJSON(rawToChar(req1$content))$data
+devices <- data.frame(deviceid = (1:length(jreq)))
+for (i in (1:length(jreq))){
+  devices$deviceid[i] <- jreq[[i]]$id
+}
+
