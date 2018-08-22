0ef3b64645e6feddd79d5a3002742c09ab3ca6d0
Gustavo Olivarescd <guolivar@gmail.com>
2018-08-22 13:51:58 +1200

    Campaign finished, now starting the analysis

---
+++ parse_offline_data.R
@@ -0,0 +1,10 @@
+#########################################################################
+# Parsing, cleaning ODIN-SD offline data with intermittent clock updates.
+# Developed for the project "Air Quality Measurements in Gisborne - 2018"
+# Authors:
+#         Gustavo Olivares
+#########################################################################
+
+# Prepare libraries
+library(librarian)
+shelf()
